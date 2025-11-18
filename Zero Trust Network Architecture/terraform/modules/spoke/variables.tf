84293c5d92faf9fb5a6e0e2a4bcee0c171b7d7a3
tee-koo <72820951+tee-koo@users.noreply.github.com>
2025-11-18 07:20:35 +0200

    Create variables.tf

---
+++ Zero Trust Network Architecture/terraform/modules/spoke/variables.tf
@@ -0,0 +1,6 @@
+variable "resource_group_name" { type = string }
+variable "location"            { type = string }
+variable "hub_vnet_id"         { type = string }
+variable "firewall_private_ip" { type = string }
+variable "spoke_name"          { type = string }
+variable "spoke_cidr"          { type = string }
